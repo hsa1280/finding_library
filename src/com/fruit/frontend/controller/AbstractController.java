7392bd94517086ae2c11bc6aba55b08c581d87b0
Shian <shianhuang2008@gmail.com>
2014-06-24 22:25:59 -0600

    changed spring version
    java.lang.ClassNotFoundException:
    org.springframework.web.servlet.DispatcherServlet
    
    Go to this link to solve this problem:
    
    http://goldenpackagebyanuj.blogspot.com/2013/03/java.lang.ClassNotFoundExceptionorg.springframework.web.servlet.DispatcherServlet.html

--- src/com/fruit/frontend/controller/AbstractController.java
+++ src/com/fruit/frontend/controller/AbstractController.java
@@ -3,19 +3,18 @@ package com.fruit.frontend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.fruit.backend.service.impl.FindingLibraryService;
-
 public class AbstractController {
 
-	@Autowired
-	private FindingLibraryService findingLibraryService;
-
-	public FindingLibraryService getFindingLibraryService() {
-		return findingLibraryService;
-	}
-
-	public void setFindingLibraryService(FindingLibraryService findingLibraryService) {
-		this.findingLibraryService = findingLibraryService;
-	}
+//	@Autowired
+//	private FindingLibraryService findingLibraryService;
+//
+//	public FindingLibraryService getFindingLibraryService() {
+//		return findingLibraryService;
+//	}
+//
+//	public void setFindingLibraryService(FindingLibraryService findingLibraryService) {
+//		this.findingLibraryService = findingLibraryService;
+//	}
 	
 	
 }
--- src/com/fruit/frontend/controller/EmployeeController.java
+++ src/com/fruit/frontend/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.fruit.frontend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fruit.backend.service.impl.FindingLibraryService;
+
 @Controller
 @RequestMapping( "/Employee" )
 public class EmployeeController extends AbstractController {
 
+	@Autowired
+	private FindingLibraryService findingLibraryService;
+
+	public FindingLibraryService getFindingLibraryService() {
+		return findingLibraryService;
+	}
+
+	public void setFindingLibraryService(FindingLibraryService findingLibraryService) {
+		this.findingLibraryService = findingLibraryService;
+	}
+	
 	@RequestMapping( value = "/list", method = RequestMethod.GET )
 	@ResponseStatus( HttpStatus.OK)
 	public ModelAndView list() throws Exception {
